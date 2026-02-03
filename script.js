03fcdba239257f8844402f73015373d495af4ee2
esztike <56134483+esztike@users.noreply.github.com>
2026-02-02 17:47:32 -0800

    feat: add content, style and interactions to pages

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // my scriopt will come here
+
+document.getElementById("sendBtn").addEventListener("click", () => {
+  alert(
+    "Thanks for your message. This demo form doesn’t send emails, but feel free to reach out on LinkedIn.",
+  );
+});
